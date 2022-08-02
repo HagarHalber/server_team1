a28ed0e1ef69e5856d91bb4aaeb786b80bb63b54
amit ganzi <ganzi@post.bgu.ac.il>
2022-08-02 09:31:52 +0300

    part 3

--- Flask Skeleton Project/static/js/markPage.js
+++
@@ -1,9 +0,0 @@
-const activePge = window.location.href;
-console.log(activePge);
-const navList = document.querySelectorAll('nav a').
- forEach(link => {
-    if(link.href == activePge){
-        console.log(activePge);
-        link.classList.add('active');
-    }
-});
--- Flask Skeleton Project/static/js/myProfileJS.js
+++ Flask Skeleton Project/static/js/myProfileJS.js
@@ -2,17 +2,6 @@
 const editButton = document.getElementById('edit');
 const saveButton = document.getElementById('save');
 
-//responsive nav bar
-const activePge = window.location.href;
-console.log(activePge);
-const navList = document.querySelectorAll('nav a').
- forEach(link => {
-    if(link.href == activePge){
-        console.log(activePge);
-        link.classList.add('active');
-    }
-});
-
 //dates validation
 var today = new Date();
 var dd = today.getDate();
